0224c9696c70cd1b8dba0752fc38a9f23c2843d9
araggsiva <raj.siva111@gmail.com>
2015-12-22 19:01:18 +0530

    updated ed2 file

--- ed2.php
+++ ed2.php
@@ -1,14 +1,9 @@
 <?php
 //session_start();
 include_once 'dbconnect.php';
-
-
-	
-	$email = mysql_real_escape_string($_POST['reg_email']);
-	
-	
-	$res=mysql_query("SELECT * FROM users WHERE email='$email'");
-	$row=mysql_fetch_array($res);
+$email = $_POST['fp_email'];
+$res=mysql_query("SELECT * FROM users WHERE email='$email'");
+	//$row=mysql_fetch_array($res);
 	$result=mysql_num_rows($res);
 	if($result == 0){
 		
@@ -16,23 +11,22 @@ include_once 'dbconnect.php';
 
 	}
 	else{
-		
-  
-  //Email information
-  $admin_email = $row['email'];
-  //$email = $_REQUEST['email'];
-  $subject = "Reset your CGS password";
-  $comment = $_REQUEST['comment'];//i need to send link here after putting in to server
-  
-  //send email
-  mail($admin_email, "$subject", $comment, "From:" . $email);
-  
-  //Email response
-  echo "Thank you for contacting us!";
-  
-  
-	}
+			$email = $_POST['fp_email'];
+  			$too = $email;
+			$subject = "CeeGees";
+			$message = "http://araggsiva.net16.net/reset.php?email=".$email;
+			$user_email = ""; // valid POST email address
+			$headers  = "From: $user_email\r\n";
+			$headers .= "Reply-To: $too\r\n";
+			$headers .= "Return-Path: $too\r\n";
+			$headers .= "X-Mailer: PHP/" . phpversion(). "\r\n";
+			$headers .= "MIME-Version: 1.0" . "\n";
+			$headers .= "Content-type: text/html; UTF-8". "\r\n";
+			if(mail($too,$subject,$message,$headers)) {
+				echo '1'; 
+			}
 	
+	}
 
 ?>
 
