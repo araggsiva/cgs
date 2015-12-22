e80395ea61b43e71e89d236941df8001a31e96e4
araggsiva <raj.siva111@gmail.com>
2015-12-22 19:00:10 +0530

    updated db file

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
-if(!mysql_connect("localhost","root",""))
+if(!mysql_connect("mysql11.000webhost.com","a9779819_cgs","cgs123"))
 {
 	die('oops connection problem ! --> '.mysql_error());
 }
-if(!mysql_select_db("cgs"))
+if(!mysql_select_db("a9779819_cgs"))
 {
 	die('oops database selection problem ! --> '.mysql_error());
 }
